f65f2cb1d918113791cd86f466b41b5ece4c8dff
nouronallah <nour.onallah1234@gmail.com>
2025-11-15 09:57:28 -0800

    Refactor split_at_first_digit function

--- string_utils.py
+++ string_utils.py
@@ -15,11 +15,10 @@ def split_before_each_uppercases(formula):
     prefix = formula[:digit_location]
     number = int(formula[digit_location:])
     return prefix, number
-    pass  # Replace the `pass` with your code
+   
 
 
 def split_at_first_digit(formula):
-    def test_split_before_each_uppercases(formula):
     start = 0
     end = 1
     split_formula = []
@@ -32,7 +31,7 @@ def split_at_first_digit(formula):
             start = end
         end += 1
 
-    # Append the last part
+  
     split_formula.append(formula[start:end])
 
     return split_formula
